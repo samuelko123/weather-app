1c52d0052ea46ef2aa196e063cc4b9521f90ede3
Samuel Ko <koshunyin@gmail.com>
2022-01-21 00:06:12 +1100

    fix scrollview not scrolling to bottom

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components/native'
 
-export const Main = styled.ScrollView.attrs(() => ({
-	contentContainerStyle: { flexGrow: 1 },
+export const Main = styled.ScrollView.attrs((props) => ({
+	contentContainerStyle: {
+		flexGrow: 1,
+		paddingBottom: props.theme.base.spacing * 4,
+	},
 }))`
     background-color: ${props => props.theme.color.background};
     padding-left: ${props => props.theme.base.spacing * 2}px;
     padding-right: ${props => props.theme.base.spacing * 2}px;
-	flex-grow: 1;
 `
